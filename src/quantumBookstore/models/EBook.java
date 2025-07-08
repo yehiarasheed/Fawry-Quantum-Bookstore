faac2284ba783a759eb40d590b7c259654a78599
Yehia Rasheed <157399068+yehiarasheed@users.noreply.github.com>
2025-07-08 21:33:34 +0300

    feat: Implement `Mailable` at the `EBook` class for further extensibility.

--- src/quantumBookstore/models/EBook.java
+++ src/quantumBookstore/models/EBook.java
@@ -1,6 +1,8 @@
 package quantumBookstore.models;
 
-public class EBook extends Book {
+import quantumBookstore.interfaces.Mailable;
+
+public class EBook extends Book implements Mailable {
     private String fileType;
 
     public EBook(String ISBN, String title, int publicationYear,double price, String fileType) {
@@ -8,4 +10,8 @@ public class EBook extends Book {
         this.fileType = fileType;
     }
 
+    @Override
+    public String getFileType() {
+        return fileType;
+    }
 }
